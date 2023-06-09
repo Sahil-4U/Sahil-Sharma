d26894acdd436e8147087c48e814a86dbe9950cf
Sahil <ss6611786@gmail.com>
2023-06-09 23:17:04 +0530

    so guys i have complete all the require functionality

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let listsEl=document.getElementsByClassName('list');
 let rightBox=document.getElementById('right');
 let leftBox=document.getElementById('left');
+let btnEl=document.getElementsByClassName('btn')[0];
 
 for(list of listsEl){
     list.addEventListener('dragstart',function(e){
         let selected=e.target;
+        selected.style.backgroundColor='aqua';
 
         rightBox.addEventListener('dragover',function(e){
             e.preventDefault();
         })
         rightBox.addEventListener('drop',function(e){
+            selected.style='none'
             rightBox.appendChild(selected);
             selected=null;
         })
@@ -18,8 +21,12 @@ for(list of listsEl){
             e.preventDefault();
         })
         leftBox.addEventListener('drop',function(e){
+            selected.style='none'
           leftBox.appendChild(selected);
             selected=null;
         })
     });
 }
+btnEl.addEventListener('click',()=>{
+   window.location.reload();
+})
