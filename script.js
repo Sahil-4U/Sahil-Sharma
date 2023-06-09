4214cefe12bf7fd1c8900a70ae3f85f40592868c
Sahil <ss6611786@gmail.com>
2023-06-09 23:29:43 +0530

    so guys i have add comments to understand this application please read and enjoy the working of this application

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ let rightBox=document.getElementById('right');
 let leftBox=document.getElementById('left');
 let btnEl=document.getElementsByClassName('btn')[0];
 
+// Approach:-What i am doing i iterate the all elements of the left box and on which box a user fire a dragstart event 
+// i find that element and at that time i stop the right box default behavior and drop that element there
+
+
+
 for(list of listsEl){
+    // loop all the elements and find the user select element
     list.addEventListener('dragstart',function(e){
         let selected=e.target;
+        // here i change the color of that element 
         selected.style.backgroundColor='aqua';
 
         rightBox.addEventListener('dragover',function(e){
+            // here i stopped the default behavior of right box to consume the selected element
             e.preventDefault();
         })
         rightBox.addEventListener('drop',function(e){
@@ -17,6 +25,7 @@ for(list of listsEl){
             selected=null;
         })
 
+        // here i am doing the same thing for left box also
         leftBox.addEventListener('dragover',function(e){
             e.preventDefault();
         })
@@ -28,5 +37,12 @@ for(list of listsEl){
     });
 }
 btnEl.addEventListener('click',()=>{
+    // when user click on reset button what i am doing is reload tha page again
    window.location.reload();
 })
+
+
+
+// how to use this project:----
+// i)Just drag the element from left box to right to understand this project and same as from right box to left box
+// ii)Press reset button to go to the initial stage of the application
